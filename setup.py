8f5fe4ed668ff8d493e426202a0b57faa6e201b0
Pedro Barbosa <jpedro.barbosa@gmail.com>
2016-11-22 15:13:16 +0100

    Target real-time supply-chains

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="backup-mysql",
+    version="0.1.3",
+    description="Backup mysql utility",
+    long_description="Backup mysql utility.",
+    url="https://github.com/ptdorf/backup-mysql",
+    author="ptdorf",
+    author_email="ptdorf@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ],
+    keywords="sample backup mysql",
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+    install_requires=["MySQL-python"],
+    entry_points={
+        "console_scripts": [
+            "backup-mysql=backup.mysql:main",
+        ],
+    },
+)
